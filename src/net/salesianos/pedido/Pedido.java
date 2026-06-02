d0558150b12da553cc7eb6d41b543218be78d44f
J44v11 <javiergarcialopez101@gmail.com>
2026-06-02 12:44:00 +0100

    feat: Agrego métodos getter y setter a la clase Pedido

--- src/net/salesianos/pedido/Pedido.java
+++ src/net/salesianos/pedido/Pedido.java
@@ -14,4 +14,37 @@ public class Pedido {
         this.paraLlevar = paraLlevar;
     }
 
+        public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public boolean isParaLlevar() {
+        return paraLlevar;
+    }
+
+    public void setParaLlevar(boolean paraLlevar) {
+        this.paraLlevar = paraLlevar;
+
+    }
+
 }
